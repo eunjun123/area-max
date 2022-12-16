b964ee2c9b55da0b77383df7e8455da38ef88e73
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 18:06:22 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+    class problem
+    {
+        public static void Main()
+        {
+            int[] Area = new int[] { 12, 15, 9, 3, 0 };
+            int[] A1 = new int[200];
+
+            Console.WriteLine("최대값:" + Area.Max());
+
+        }
+    }
